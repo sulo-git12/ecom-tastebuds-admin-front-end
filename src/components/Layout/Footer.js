564c89d805b2c190474f22e12e4446117fe174e1
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-12 08:48:40 +0530

    Add footer

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        © Copyright © 2022 - NIBM Software Solutions Lanka (Pvt) Ltd. All rights
+        reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
